bb32da574ae1d40aead574cc8e1fc28eb0b0df25
Your Name <you@example.com>
2022-01-20 15:46:22 +0200

    Inheritance V

--- oop.js
+++ oop.js
@@ -272,7 +272,41 @@ class Dog {
 //       new Nurse('Olynyk', ['Trauma', 'Pediatrics']); 
 //   console.log(nurseOlynyk);
 //-------------------Inheritance IV--------------------------//  
-class HospitalEmployee {
+// class HospitalEmployee {
+//     constructor(name) {
+//       this._name = name;
+//       this._remainingVacationDays = 20;
+//     }
+    
+//     get name() {
+//       return this._name;
+//     }
+    
+//     get remainingVacationDays() {
+//       return this._remainingVacationDays;
+//     }
+    
+//     takeVacationDays(daysOff) {
+//       this._remainingVacationDays -= daysOff;
+//     }
+//   }
+  
+//   class Nurse extends HospitalEmployee {
+//    constructor(name, certifications) {
+//      super(name);
+//      this._certifications = certifications;
+//    } 
+//   }
+  
+//   const nurseOlynyk = new
+//                       Nurse(
+//                        'Olynyk',
+//                        ['Trauma','Pediatrics']);
+//   var remainingVacationDays = nurseOlynyk.takeVacationDays(5);
+//   console.log(nurseOlynyk.remainingVacationDays);
+
+  //----------------------Inheritance V-------------------------//
+  class HospitalEmployee {
     constructor(name) {
       this._name = name;
       this._remainingVacationDays = 20;
@@ -292,16 +326,24 @@ class HospitalEmployee {
   }
   
   class Nurse extends HospitalEmployee {
-   constructor(name, certifications) {
-     super(name);
-     this._certifications = certifications;
-   } 
+    constructor(name, certifications) {
+      super(name);
+      this._certifications = certifications;
+    } 
+  
+    get certifications()
+    {
+      return this._certifications;
+    }
+  
+    addCertification(newCertification){
+        this._certifications.push(newCertification);
+     }
   }
   
-  const nurseOlynyk = new
-                      Nurse(
-                       'Olynyk',
-                       ['Trauma','Pediatrics']);
-  var remainingVacationDays = nurseOlynyk.takeVacationDays(5);
+  const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+  nurseOlynyk.takeVacationDays(5);
   console.log(nurseOlynyk.remainingVacationDays);
+  nurseOlynyk.addCertification('Genetics');
+  console.log(nurseOlynyk.certifications);
   

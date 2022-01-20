5c79fe15763538fe3983aa0d500387a958e1303f
Your Name <you@example.com>
2022-01-20 12:14:17 +0200

    OOP

--- oop.js
+++ oop.js
@@ -20,6 +20,12 @@
          this._subject = subject;
          this._nationality = nationality;
      }
+
+     call(){
+         //console.log("Omer is here, He needs a hand here");
+
+         console.log("omer " + this._nationality);
+     }
  }
 
  const jeton = new Student
@@ -47,3 +53,68 @@ console.log(rachel._nationality);
 console.log(omer._nationality);
 
 console.log(rachel._subject === moses._subject);
+
+console.log(omer._name + " is from " + omer._nationality);
+
+omer.call();
+
+class Prestudent extends Student {
+    study() {
+        console.log("I am trying to learn");
+    }
+}
+
+const maria = new Prestudent("Maria", "Cybersecurity", "Spain");
+
+maria.call();
+maria.study();
+
+//----------Introduction to Classes----------//
+class Dog {
+    constructor(name) {
+      this._name = name;
+      this._behavior = 0;
+    }
+  
+    get name() {
+      return this._name;
+    }
+    get behavior() {
+      return this._behavior;
+    }   
+  
+    incrementBehavior() {
+      this._behavior ++;
+    }
+  }
+  
+  const halley = new Dog('Halley');
+  console.log(halley.name); // Print name value to console
+  console.log(halley.behavior); // Print behavior value to console
+  halley.incrementBehavior(); // Add one to behavior
+  console.log(halley.name); // Print name value to console
+  console.log(halley.behavior); // Print behavior value to console
+
+  //----------------Constructor---------------//
+  //class Surgeon{
+    //constructor(name, department){
+      //this._name = name;
+      //this._department = department;
+    //}
+  //}
+  
+
+  //-----------------Instance------------------//
+  class Surgeon {
+    constructor(name, department) {
+      this.name = name;
+      this.department = department;
+    }
+  }
+  
+  const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular')
+  
+  const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics')
+
+  console.log(surgeonRomero);
+  

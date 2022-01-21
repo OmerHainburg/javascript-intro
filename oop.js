b3cc11ba5eb726dc398abbeb6f0802ee6983351f
Your Name <you@example.com>
2022-01-21 16:45:05 +0200

    oop

--- oop.js
+++ oop.js
@@ -726,7 +726,43 @@ const howOld = (age, year) => {
     }
     return 'You are likely not related.'
 }
-
 console.log(whatRelation(34))
 
 console.log(whatRelation(3))
+
+//--------------tipCalculator()-------------//
+const tipCalculator = (quality, total) => {
+  switch (quality) {
+      case 'bad':
+          return total * .05;
+      case 'ok':
+          return total * .15;
+      case 'good':
+          return total * .20;
+      case 'excellent':
+          return total * .30;
+      default:
+          return total * .18;
+
+  }
+}
+/*
+// As a function declaration:
+function tipCalculator(quality, total) {
+  switch (quality) {
+      case 'bad':
+          return total * .05;
+      case 'ok':
+          return total * .15;
+      case 'good':
+          return total * .20;
+      case 'excellent':
+          return total * .30;
+      default:
+          return total * .18;
+
+  }
+}
+*/
+console.log(tipCalculator('good', 100))
+

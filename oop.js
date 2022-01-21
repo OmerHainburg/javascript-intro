39bf34f77dbff71f53ddd0a45a9bb8a99cf404db
Your Name <you@example.com>
2022-01-21 16:46:40 +0200

    toEmoticon

--- oop.js
+++ oop.js
@@ -766,3 +766,41 @@ function tipCalculator(quality, total) {
 */
 console.log(tipCalculator('good', 100))
 
+//---------------toEmoticon()--------------//
+const toEmoticon = meaning => {
+  switch (meaning) {
+      case 'shrug':
+          return '|_{"}_|'
+      case 'smiley face':
+          return ':)';
+      case 'frowny face':
+          return ':(';
+      case 'winky face':
+          return ';)';
+      case 'heart':
+          return '<3';
+      default:
+          return '|_(* ~ *)_|';
+  }
+}
+/*
+// As a function declaration: 
+function toEmoticon(meaning) {
+  switch (meaning) {
+      case 'shrug':
+          return '|_{"}_|'
+      case 'smiley face':
+          return ':)';
+      case 'frowny face':
+          return ':(';
+      case 'winky face':
+          return ';)';
+      case 'heart':
+          return '<3';
+      default:
+          return '|_(* ~ *)_|';
+  }
+}
+*/
+console.log(toEmoticon("whatever"))
+

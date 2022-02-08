77a5e65254bf1c9c76334cfc611ef69e31e6d030
Your Name <you@example.com>
2022-02-08 11:44:56 +0200

    Commit

--- index.js
+++ index.js
@@ -149,5 +149,79 @@ function spEng(sentence){
       return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
     }
 
-   
+    //Our football team finished the championship. The result of each match look like "x:y".
+    // Results of all matches are recorded in the collection.
+    //For example: ["3:1", "2:2", "0:1", ...]
+    //Write a function that takes such collection and counts the points of our team in the championship.
+    // Rules for counting points for each match: if x>y - 3 points if x<y - 0 point if x=y - 1 point
+     function points(games) {
+      return games.reduce((output,current)=>{
+        let x = parseInt(current[0]);
+        let y = parseInt(current[2]);
+        let value= x>y ? 3 : x===y ? 1 : 0;
+        return output+value;
+        },0)
+      }
 
+      //points(5);
+
+      //You are given an odd-length array of integers,
+      //in which all of them are the same, except for one single number.
+      //Complete the method which accepts such an array, and returns that single different number.
+      //The input array will always be valid! (odd-length >= 3)
+      function stray(numbers){
+        for (var i in numbers){
+           if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){return numbers[i]}
+        }
+      }
+   
+      stray(1,1,2);
+
+      //At the annual family gathering, the family likes to find the oldest living family
+      //member’s age and the youngest family member’s age and calculate the difference between them.
+      //You will be given an array of all the family members' ages, in any order. 
+      //The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0.
+      //Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+      function differenceInAges (ages) {
+
+        let max = Math.max(...ages),
+            min = Math.min(...ages)
+            diff = max - min
+            
+        return [min, max, diff]
+    }
+
+    Challenge:
+
+//Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+//Example:
+
+//Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+//Addendum:
+
+//Please, keep in mind, that JavaScript is by default sorting objects alphabetically. For more information, please consult:
+
+//http://stackoverflow.com/questions/6093874/why-doesnt-the-sort-function-of-javascript-work-well
+
+"use strict";
+
+function flattenAndSort(array) {
+  return [].concat(...array).sort((a,b) => a - b);
+}
+
+
+//Let's create some scrolling text!
+
+//Your task is to complete the function which takes a string, and returns an array with all
+// possible rotations of the given string, in uppercase.
+function scrollingText(text){
+  let result = []
+  
+  for( var i = 0; i < text.length; i++){
+    result.push((text.slice(i) + text.slice(0,i)).toUpperCase())
+  }
+  
+  return result
+}

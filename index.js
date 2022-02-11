428d5820e1e178025d8b08603ffa81d468aa229a
Your Name <you@example.com>
2022-02-11 12:26:31 +0200

    Commit

--- index.js
+++ index.js
@@ -225,3 +225,68 @@ function scrollingText(text){
   
   return result
 }
+
+//Write a function to convert a name into initials.
+//This kata strictly takes two words with one space in between them.
+//The output should be two capital letters with a dot separating them.
+function abbrevName(name){
+
+  var nameArray = name.split(" ");
+  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
+
+//What is the flag?
+//The flag is hidden in the string variable FLAG
+//The flag is in the format FLAG{CAPITAL-LETTERS-WITH-HYPHEN}
+//The flag couldn't be found from the string literal right away... but You can indeed read it somehow!
+//A magic number 16 might help... but how should it be used?
+let flag = "FLAG{IS-IT-EASY-YET-FUN}";
+const FLAG = "dflaskjf; klcqwjer9qieiqucwepirom9028905839045809ds8gf[90g8sd[fh8f-[gh8-09wer0=t]mxcvnmz.xvchku9erpiot789-348tipahjrgfkhzxvxmb,mncxmh6t3q90485ut9rsqklgfjkj#Q$YTEWOPJGPOEDSJC)$%#@^*@$%690uiodrj;lstj3;wpiot8ujdfpgioja90-8-0cx8v#KL%J,,.345.3.l0-0----0000-00---0-----0..-.-.--.....--.-.-.----5345..9.........4.....3.........456...4.........3.....2.........7.....3424.......................564...544......4...5.8...9.....6...3.7.........79345.9.565.....1...1.3...7.....1...1.33452.......................013..89345......4......9......732......8........6......8........9......9........846..78654......................698............8...3............6.....098345.....73............6...6............186...........................79843..9879878....6.......7.......8.......5.......8.......6.......9.......9.....12387.....7............................786982..........8........45623..77833...........9...............109841....................345.....723....5...1...6...2...6...3....5......78234.....45....1...8...7...8...2...3....873....4...5...........................4.....7..........1...5............9.4...93459......1...............1...............1............................4.....3..345634..3...7...3........7.5....3455......1.....3.........2.....867843....1............................5321452............1...............1...............1....743745.....1...............1............................2345754..5.....44........6.....354335....3.....13........4.....42........7542345................4....2...533....52...3.....6....2.3..9.....98...1..7.6.....6....7...88.....5....6....9...964....................------0--0-=0-=-0-00000000-00-0-0-000000,0,,0000jasdkl;jferiotupvn23c014985901c79840p72x812m598723189057m2190857mx98012479812798m1407395813479857m2149857198x0579814320758937981982347384957983g"
+console.log(FLAG);
+
+//Given two arrays of integers m and n, test if they contain at least one identical element.
+//Return true if they do; false if not.
+//Your code must handle any value within the range of a 32-bit integer,
+//and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
+function duplicateElements(m, n) {
+  console.log(m.some(v => n.includes(v)));
+}
+
+duplicateElements([1,2,3], [2,5,6]);
+
+//Ben has a very simple idea to make some profit: he buys something and sells it again.
+//Of course, this wouldn't give him any profit at all if he was simply to buy and sell
+//it at the same price. Instead, he's going to buy it for the lowest possible price and
+//sell it at the highest.
+//Task
+//Write a function that returns both the minimum and maximum number of the given list/array.
+function minMax(arr){
+  console.log([Math.min(...arr), Math.max(...arr)]);
+}
+minMax([1,5,3,8]);
+
+
+//You have an amount of money a0 > 0 and you deposit it with an interest rate of p percent
+//divided by 360 per day on the 1st of January 2016. You want to have an amount a >= a0.
+//Task:
+//The function date_nb_days (or dateNbDays...) with parameters a0, a, p will return,
+//as a string, the date on which you have just reached a.
+
+// Example:
+// date_nb_days(100, 101, 0.98) --> "2017-01-01" (366 days)
+// date_nb_days(100, 150, 2.00) --> "2035-12-26" (7299 days)
+// Notes:
+// The return format of the date is "YYYY-MM-DD"
+// The deposit is always on the "2016-01-01"
+// Don't forget to take the rate for a day to be p divided by 36000 since banks consider that there are 360 days in a year.
+// You have: a0 > 0, p% > 0, a >= a0
+
+const ln = Math.log;
+
+function dateNbDays(a0, a, p) {
+  let days = Math.ceil((ln(a) - ln(a0))/ln(1 + p/36000));
+  var date = new Date(2016, 0, 1 + days);
+  return date.toISOString().split('T')[0];
+}
+
+
